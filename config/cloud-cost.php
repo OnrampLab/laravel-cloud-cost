1ffb1712e6e0535871cb49a14c5c8fea7b5bbc91
glenn guan <glenn.profile@gmail.com>
2023-07-11 16:56:42 +0800

    chore(config): [cloud-cost] driver and provider setting

--- config/cloud-cost.php
+++ config/cloud-cost.php
@@ -3,29 +3,36 @@
 return [
     /*
     |--------------------------------------------------------------------------
-    | Default Key Provider Name
+    | custom config
     |--------------------------------------------------------------------------
-    |
-    */
-    'default' => env('CLOUD_COST_PROVIDERS', ['aws']),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Key Providers
-    |--------------------------------------------------------------------------
-    |
-    | Configure the provider information for each service that
-    | is used by your application.
-    |
-    | Drivers: "local", "aws"
     |
     */
     'providers' => [
-        'aws' => [
+        'main-setting' => [
             'driver' => 'aws',
+            'config' => [
+                'filter' => [
+                    'Tags' => [
+                        'Key' => 'Project',
+                        'Values' => ['YourProjectName', 'your-project-name'],
+                    ],
+                ],
+            ]
+        ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | drivers
+    |--------------------------------------------------------------------------
+    |
+    */
+    'driver' => [
+        'aws' => [
             'access_key' => env('AWS_ACCESS_KEY_ID'),
             'access_secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION', 'us-west-1'),
+            'version' => '2017-10-25',
         ],
     ],
 ];
